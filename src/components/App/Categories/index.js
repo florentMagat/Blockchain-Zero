4bcff00cb2a8e7faf69bf7b93ce60539f77aecf3
Florent Flo <adressemail22000@gmail.com>
2023-02-09 10:50:12 +0100

    mise en place de CRA à la place de la configuration proposée par la formation

--- src/components/App/Categories/index.js
+++ src/components/App/Categories/index.js
@@ -6,15 +6,15 @@ import './styles.scss';
 function Categories() {
     return (
         <div className='categories'>
-          <Button variant="primary" size="sm">Infrastructure</Button>{' '}
-          <Button variant="secondary" size="sm">Stablecoins</Button>{' '}
-          <Button variant="success" size="sm">Defi</Button>{' '}
-          <Button variant="warning" size="sm">Exchanges</Button>{' '}
-          <Button variant="danger" size="sm">NFT</Button>{' '}
-          <Button variant="info" size="sm">Gaming</Button>{' '}
+          <Button variant="warning" size="sm">Classement par capitalisations</Button>{' '}
+          <Button variant="primary" size="sm">Classement par capitalisations (croissant)</Button>{' '}
+          <Button variant="success" size="sm">Plus fortes hausses (24 H)</Button>{' '}
+          <Button variant="danger" size="sm">Plus fortes baisses (24 H)</Button>{' '}
+          {/* <Button variant="info" size="sm">Gaming</Button>{' '}
           <Button variant="light" size="sm">Memes Coins</Button>{' '}
-          <Button variant="dark" size="sm">Autres</Button>
-          {/* <Button variant="link" size="sm">Link</Button> */}
+          <Button variant="dark" size="sm">Autres</Button>          
+          <Button variant="secondary" size="sm">Stablecoins</Button>{' '}
+          <Button variant="link" size="sm">Link</Button> */}
         </div>
       );
     }
--- src/components/App/Result/index.js
+++ src/components/App/Result/index.js
@@ -7,10 +7,15 @@ import '../../../data/websites.json';
 
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import classNames from 'classnames';
 
 function Result({ name, image, current_price, market_cap_rank, price_change_percentage_24h }) {
-
   const priceChange = Math.round(price_change_percentage_24h*100)/100;
+  const evolution = classNames({
+    'evolution-green': priceChange > 0,
+    'evolution-null': priceChange === 0,
+    'evolution-red': priceChange < 0,
+  });
 
   return (
     <Card className="result" style={{ width: '18rem' }}>
@@ -19,9 +24,11 @@ function Result({ name, image, current_price, market_cap_rank, price_change_perc
         <Card.Img variant="top" src={image} className="result_image" />
       </div>
       <Card.Body>
-        <Card.Title>{ name }</Card.Title>
-        <Card.Text className="evolution">Evolution (24H) : {priceChange} %</Card.Text>
-        <Card.Text>Prix actuel : ${current_price}</Card.Text>
+        <Card.Title className="result_title">{ name }</Card.Title>
+        <Card.Text >
+          Evolution (24H) : <strong className={evolution}>{priceChange} %</strong>
+        </Card.Text>
+        <Card.Text>Prix actuel : <strong>{current_price} $</strong></Card.Text>
         <Button variant="btn btn-outline-primary">Plus d'informations</Button>
       </Card.Body>
     </Card>
