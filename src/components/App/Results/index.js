a2f7d5607cb862beb6fcb5714bc67ee612f395bb
Florent Flo <adressemail22000@gmail.com>
2023-02-17 21:29:08 +0100

    ajout connexion API avec axios

--- src/components/App/Results/index.js
+++ src/components/App/Results/index.js
@@ -1,21 +1,33 @@
 /* eslint-disable linebreak-style */
-import { useEffect, useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
+import axios from 'axios';
 
 import Result from '../Result';
 
-import axios from 'axios';
+const baseURL = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=24h';
 
-import data from '../../../data/coins.json';
+// import data from '../../../data/coins.json';
 
 function Results() {
+  const [data, setData] = React.useState(null);
+
+  React.useEffect(() => {
+    axios.get(baseURL).then((response) => {
+      setData(response.data);
+    });
+  }, []);
+
+  if (!data) return null;
+
   return (
     data.map((coin) => (
       <Result
         key={coin.id}
         {...coin}
       />
-    )));
+    ))
+  );
 }
 
 Results.propTypes = {
